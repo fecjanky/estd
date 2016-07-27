189029dd0fe095c048a916515cda73129ff3ff79
Ferenc Janky <fecjanky@gmail.com>
2016-07-27 23:08:15 +0200

    poly_vector file added

---
+++ include/poly_vector.h
@@ -0,0 +1,18 @@
+#ifndef POLY_VECTOR_H_
+#define POLY_VECTOR_H_
+
+#include "memory.h"
+
+namespace estd {
+
+    template<class IF>
+    class poly_vector {
+    public:
+    private:
+
+    };
+
+}  // namespace estd
+
+#endif  //POLY_VECTOR_H_
+ 
